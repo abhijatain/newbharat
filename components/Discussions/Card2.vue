1790913533a3620417c5baf6521bc7d26bda1484
abhijatain <abhi31jatain@gmail.com>
2024-05-26 23:16:45 +0530

    4th Commit

--- components/Discussions/Card2.vue
+++ components/Discussions/Card2.vue
@@ -2,8 +2,8 @@
     <div class="bg-white p-3 dark:bg-black dark:text-white rounded-lg flex flex-col items-center justify-between md:flex-row gap-3  ">
         <div class="flex flex-row gap-4 rounded-xl md:hidden  my-auto w-full text-start justify-between" >
                 <div class="flex flex-row gap-4   my-auto text-start">
-                    <img class="rounded-xl h-8 md:h-12 " src="https://static.vecteezy.com/system/resources/previews/022/448/291/non_2x/save-earth-day-poster-environment-day-nature-green-ai-generative-glossy-background-images-tree-and-water-free-photo.jpg" alt="Jese image">
-                <p class="justify-start text-xs md:hidden w-full ">abhijatain <br> May 12, 2024</p>
+                    <img class="rounded-xl h-8 md:h-12 " src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Jese image">
+                <p class="justify-start text-xs md:hidden w-full "><span class="font-bold">abhijatain</span> <br> May 12, 2024</p>
                 </div>
                 <div>
                     <Button icon="pi pi-bookmark" severity="secondary" text rounded aria-label="Bookmark" />
@@ -16,15 +16,33 @@
                <h5 class=" font-bold mb-1 ">
                    Best way to have GIF favicon but icon in all browsers. Best way to have GIF favicon but icon in all browsers?
                </h5>
+               <div>
+                <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Canada</span>
+                <span class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">Sports</span>
+                <span class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">Football</span>
+                <span class="bg-indigo-100 text-indigo-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300">Indigo</span>
+
+                </div>
            </div>
      
         <div class="md:flex flex-row gap-3 hidden " >
-            <img class=" h-8 md:h-10 mx-auto rounded-full ring-2 ring-white dark:ring-gray-900 md:inline-block md:my-auto" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+            <img class=" h-8 md:h-10 mx-auto rounded-full ring-2 ring-white dark:ring-gray-900 md:inline-block md:my-auto" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="" />
             <div class="">
-                <p class=" text-xs mb-2">abhijatain | May 12, 2024</p>
+                <div class="flex flex-col gap-1 p-2">
+                    <p class=" text-xs mb-2"><span class="font-bold">abhijatain</span> | May 12, 2024</p>
+                    <div>
+                        <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Canada</span>
+                        <span class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">Sports</span>
+                        <span class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">Football</span>
+                        <span class="bg-indigo-100 text-indigo-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300">Indigo</span>
+                    </div>
+
+                </div>
+                
                 <h5 class=" font-bold mb-2 ">
                     Best way to have GIF favicon but icon in all browsers?Best way to have GIF favicon but icon in all browsers?
                 </h5>
+        
             </div>
         </div>
        <div class="md:flex -space-x-1 overflow-hidden hidden w-40">
@@ -36,7 +54,7 @@
        
        
       
-        <div class="flex flex-row gap-2 w-full md:w-40">
+        <div class="flex flex-row justify-center gap-2 w-full md:w-40">
             <div class="md:block flex flex-row my-2 px-1 border rounded-lg text-xs items-center">
                <p class="p-1 font-bold text-xs md:text-center">
                    235
@@ -56,6 +74,6 @@
         
            
        </div>
-       
+     
      </div>
 </template>
