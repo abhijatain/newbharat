9ee42703bd2283175991a4e55e8b3d31039625a5
abhijatain <abhi31jatain@gmail.com>
2024-05-26 17:54:15 +0530

    4th Commit

--- pages/discussions/add.vue
+++ pages/discussions/add.vue
@@ -43,7 +43,7 @@
                 </template>
                 <template #content="{ prevCallback }">
                     <div class="shadow-lg md:m-2 rounded-lg">
-            <DiscussionsCard />
+            <DiscussionsCard2 />
             <div class="flex flex-col gap-2 p-3 md:flex-row">
                 <p class="dark:text-white ">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
