5cdfcfe3cd17cc4c41c334f8ba8b5225c154ab39
abhijatain <abhi31jatain@gmail.com>
2024-07-30 19:32:55 +0530

    Loader

--- pages/discussions/index.vue
+++ pages/discussions/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class=" container mx-auto flex flex-col gap-2 md:p-6 ">
-        <TopHeader />
+        <TopHeader class="hidden md:block"/>
     <DiscussionsCard3 class="shadow-lg dark:shadow-blue-800/50 shadow-blue-500/50"/>
     <DiscussionsCard3 class="shadow-lg dark:shadow-blue-800/50 shadow-blue-500/50"/>
     <DiscussionsCard3 class="shadow-lg dark:shadow-blue-800/50 shadow-blue-500/50"/>
